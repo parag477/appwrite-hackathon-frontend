1fb2a80261cb727f1d28be61da70f36e441d8695
Parag <paragagrawal477@gmail.com>
2023-06-09 14:21:09 +0530

    Completed home section🤗

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import './App.css';
+import Home from './components/Home';
 
 function App() {
   return (
     <div className="App">
-        <h1 className="text-3xl font-bold underline">
-          Hello world!
-        </h1>
+          <Home />
     </div>
   );
 }
